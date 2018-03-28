437736bf17ae87e7ab78a60208249f005e5a074d
Tomi <tomi_emmen@hotmail.com>
2018-03-28 08:10:41 +0000

    added doe mee change, fixed and tested backend youtube embed and changed templates to accomodate

--- resources/views/frontend/doer/create.blade.php
+++ resources/views/frontend/doer/create.blade.php
@@ -16,15 +16,14 @@
         <div class="col-lg-12 button-container">
             <div class="row" style="height: 100%;">
                 <a href="" class="col-lg-12 col-md-4 col-sm-4 col-xs-12 button experimenteerbuurt selected" data-number="0" >
-                    <span>DOENER?</span>
+                    <span>DOE MEE!</span>
                 </a>
             </div>
         </div>
         <!--  content -->
-        <div class="col-lg-12 content-area no-padding-margin">
-            <div class="col-12 no-padding-margin doener-info-video" style="height: 100%;">
-                <div id="doener-video" style="height: 100%; width: 100%;"></div>
-            </div>
+        <div class="col-lg-12 content-area no-padding-margin d-flex align-v justify-c">
+            <p class="col-10 invisible">Het meest belangrijke opgave voor de buurt op dit moment is een nieuwe locatie. Dus heb je daar ideeen over en wil je hierin actief meezoeken? Meld je! <br/> <br /> Natuurlijk zijn we optimistisch en gaan we ervanuit dat het lukt een nieuwe plek te vinden. Wij steken erop in dat de Experimenteerbuurt dan ook de kans geeft voor 20 mensen om in de buurt te gaan wonen. Wil je hier wil wonen en ook samen de plannen hiervoor uitwerken? Meld je! <br/> <br />
+Ben je bedrijf en wil je je prototype of ander experiment uitvoeren? Dan kun jij in de buurt ook niet ontbreken. Meld je aan voor de nieuwsbrief, volg ons op <a href="https://www.facebook.com/Experimenteerbuurt/" target="_blank">facebook</a> en blijf op de hoogte!</p>
         </div>
     </div>
 </div>
