7901063a96b7288768f242a8d96a49f3d3cece14
vagrant <vagrant@homestead>
2018-05-31 13:44:10 +0000

    added site verification

--- resources/views/experimenteerbuurt-layout.blade.php
+++ resources/views/experimenteerbuurt-layout.blade.php
@@ -5,7 +5,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+        <meta name="google-site-verification" content="1VxYnWI5GgxLS0IalYHQFPF7DOrKZ1IflN7FyQ2oIRY" />
+        
         <title>Experimenteerbuurt</title>
 
         <!-- bootstrap css -->
